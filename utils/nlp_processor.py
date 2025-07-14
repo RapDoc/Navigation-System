039ae1fa77ed358fa6482ba1e84686ac90e5113a
Sahaj kapoor <31739394+RapDoc@users.noreply.github.com>
2025-07-14 12:35:31 +0530

    Update nlp_processor.py

--- utils/nlp_processor.py
+++ utils/nlp_processor.py
@@ -1,4 +1,12 @@
 import spacy
+from spacy.cli import download
+
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
+
 
 nlp = spacy.load("en_core_web_sm")
 ruler = nlp.add_pipe("entity_ruler", before="ner")
